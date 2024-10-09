c699bbf3b4c255403adc638af80faf99129565bd
Francisco Tevez <franciscoraultevez@gmail.com>
2024-10-09 00:42:07 -0300

    Added home basic html+styles

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,11 @@
+import './Home.css'
+
 const Home = () => {
     return (
-        <h1>HOME</h1>
+        <section id='home'>
+            <h1 id='home-title'>Where the right sounds converge</h1>
+            <img src="https://get.wallhere.com/photo/black-musical-instrument-technology-synthesizer-Synthpop-2560x1600-px-electronic-instrument-musical-keyboard-electronic-keyboard-analog-synthesizer-545336.jpg" alt="home background" />
+        </section>
     )
 }
 export default Home
