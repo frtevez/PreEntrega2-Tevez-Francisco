4d7c949bf1e9859d530eda695fb2d295c3244e16
Francisco Tevez <franciscoraultevez@gmail.com>
2024-10-10 02:12:27 -0300

    Added remove icon for cart items

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -3,10 +3,14 @@ import { CartContext } from "../../context/cartContext"
 import "./CartItem.css"
 import { Counter } from "../Counter/Counter"
 import { useCurrency } from "../../hooks/useCurrency"
+import { NavLink } from "react-router-dom"
+import { RemoveCircleOutline } from "@mui/icons-material"
 
 const CartItem = ({ product }) => {
-    const { name, price, amount, img } = product
-    const { removeProduct, addOrSubtractProduct } = useContext(CartContext)
+    const { id, section, name, price, amount, img } = product
+    const { products, removeProduct, addOrSubtractProduct } = useContext(CartContext)
+
+    const matchIDs = p => p.id == id
 
     const increaseAmount = () => {
         addOrSubtractProduct(product, 'add')
@@ -16,13 +20,20 @@ const CartItem = ({ product }) => {
         addOrSubtractProduct(product, 'subtract')
     }
 
+    const handleRemoveFromCart = (e) => {
+        e.preventDefault()
+        removeProduct(products.findIndex(matchIDs))
+    }
+
     return (
         <article className="cart-product">
             <img src={img} alt="" />
-            <h4>{name}</h4>
+            <NavLink to={`/${section}/${id}`}>
+                <h4>{name}</h4>
+            </NavLink>
             <Counter label="Amount" number={amount} increaseFunction={increaseAmount} decreaseFunction={decreaseAmount} />
             <p><strong>{useCurrency(price, "USD")}</strong></p>
-
+            {products.some(matchIDs) && <RemoveCircleOutline className="cart-remove-icon" onClick={handleRemoveFromCart} />}
         </article>
     )
 }
